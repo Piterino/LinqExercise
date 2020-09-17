317278698c3f108878171e6aa327decdb111f657
Piterino <peter.lyskawa1995@gmail.com>
2020-09-17 14:49:06 +0200

    Initial Commit

---
+++ LinqExercise.Lib/Company.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LinqExercise.Lib
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public decimal? Value { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ LinqExercise.Lib/SampleData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqExercise.Lib
+{
+    class SampleData
+    {
+        static List<Company> LoadSampleData()
+        {
+            return new List<Company> 
+            { 
+                new Company{ Name = "Apple",     Value = 15.50m, Date = new DateTime(2020, 07, 01) },
+                new Company{ Name = "Microsoft", Value = 20,     Date = new DateTime(2020, 07, 01) },
+                new Company{ Name = "Google",    Value = 30,     Date = new DateTime(2020, 07, 01) },
+                new Company{ Name = "Nokia",     Value = 13.4m,  Date = new DateTime(2020, 07, 01) },
+                new Company{ Name = "Microsoft", Value = 21,     Date = new DateTime(2020, 07, 02) },
+                new Company{ Name = "Apple",     Value = 25,     Date = new DateTime(2020, 07, 02) },
+                new Company{ Name = "Google",    Value = 14,     Date = new DateTime(2020, 07, 02) },
+                new Company{ Name = "Google",    Value = 35,     Date = new DateTime(2020, 07, 03) },
+                new Company{ Name = "Microsoft", Value = 35,     Date = new DateTime(2020, 07, 03) },
+                new Company{ Name = "Nokia",     Value = 11,     Date = new DateTime(2020, 07, 03) },
+                new Company{ Name = "IBM",       Value = 20,     Date = new DateTime(2020, 07, 04) },
+                new Company{ Name = "William Hill", Value = 22,  Date = new DateTime(2020, 07, 04) },
+                new Company{ Name = "Apple",     Value = 21,     Date = new DateTime(2020, 07, 04) },
+                new Company{ Name = "Google",    Value = 30,     Date = new DateTime(2020, 07, 05) },
+                new Company{ Name = "IBM",                       Date = new DateTime(2020, 07, 06) }
+            };
+        }
+    }
+}
